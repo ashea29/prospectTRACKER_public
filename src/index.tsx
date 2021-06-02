760657b63340d23476a0022f79f603830d10bc2d
ashea27 <shea.andrew27@gmail.com>
2021-06-01 21:57:26 -0600

    another edit to correct deploy error

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,13 @@ import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
 import { createFirestoreInstance } from "redux-firestore"
 import store from './state/configureStore'
 import firebase from "firebase/app"
+import fbconfig from './state/fbconfig'
 import "firebase/auth"
 import "firebase/firestore"
 import App from './App'
 import AuthSpinner from './shared/AuthSpinner'
 import { useAppSelector } from './state/hooks'
 
-
-const fbConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT
-}
 
 
 const rrfConfig = {
@@ -29,7 +20,7 @@ const rrfConfig = {
   oneListenerPerPath: true
 };
 
-firebase.initializeApp(fbConfig)
+firebase.initializeApp(fbconfig)
 firebase.firestore()
 
 
